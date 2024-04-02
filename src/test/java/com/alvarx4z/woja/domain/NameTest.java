8ed66840846dc6fd570a0dcbdf7426c7848c7092
Álvaro de Francisco <alfrasan@gmail.com>
2024-04-02 21:56:39 +0200

    Add Unit Test

--- src/test/java/com/alvarx4z/woja/domain/NameTest.java
+++ src/test/java/com/alvarx4z/woja/domain/NameTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 final class NameTest {
 
@@ -15,5 +16,14 @@ final class NameTest {
 
         assertThat(name).isInstanceOf(Name.class);
         assertThat(name.getValue()).isEqualTo(NameHelper.A_NAME.getValue());
+    }
+
+    @Test
+    @DisplayName("Should throw IllegalArgumentException when null value")
+    void givenNameWithNullValue_whenInstantiating_thenThrowIllegalArgumentException() {
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> new Name(null)
+        );
     }
 }
