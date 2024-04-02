4665d7d98df08fd75cd1ae108aeb4541de611b9c
Álvaro de Francisco <alfrasan@gmail.com>
2024-04-02 21:56:23 +0200

    Add NameTest

---
+++ src/test/java/com/alvarx4z/woja/domain/NameTest.java
@@ -0,0 +1,19 @@
+package com.alvarx4z.woja.domain;
+
+import com.alvarx4z.woja.helper.NameHelper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+final class NameTest {
+
+    @Test
+    @DisplayName("Should instantiate correctly a Name")
+    void givenName_whenInstantiating_thenInstantiatedCorrectly() {
+        Name name = NameHelper.A_NAME;
+
+        assertThat(name).isInstanceOf(Name.class);
+        assertThat(name.getValue()).isEqualTo(NameHelper.A_NAME.getValue());
+    }
+}
