3e013e1f1731644f7ca65c68e69149d6752d423e
Álvaro de Francisco <alfrasan@gmail.com>
2024-04-02 21:55:27 +0200

    Add NameHelper

---
+++ src/test/java/com/alvarx4z/woja/helper/NameHelper.java
@@ -0,0 +1,10 @@
+package com.alvarx4z.woja.helper;
+
+import com.alvarx4z.woja.domain.Name;
+
+public class NameHelper {
+
+    private static final String A_VALUE = "Vol'jin";
+
+    public static final Name A_NAME = new Name(A_VALUE);
+}
