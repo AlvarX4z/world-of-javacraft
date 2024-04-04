0aa5a074857798c891eaa94bbf76c56703153c6a
Álvaro de Francisco <alfrasan@gmail.com>
2024-04-05 01:04:21 +0200

    Transform into Abstract Class

--- src/main/java/com/alvarx4z/woja/domain/invariants/PositiveInteger.java
+++ src/main/java/com/alvarx4z/woja/domain/invariants/PositiveInteger.java
@@ -1,20 +1,9 @@
 package com.alvarx4z.woja.domain.invariants;
 
-public final class PositiveInteger {
+public abstract class PositiveInteger {
 
-    private final int value;
-
-    private PositiveInteger(int value) {
+    protected PositiveInteger(int value) {
         validate(value);
-        this.value = value;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public static PositiveInteger of(int value) {
-        return new PositiveInteger(value);
     }
 
     private void validate(int value) {
