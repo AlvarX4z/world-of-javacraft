f1a2d4b820c07832abe3012922180ff023bd7018
Álvaro de Francisco <alfrasan@gmail.com>
2024-04-05 15:23:56 +0200

    Add ExpansionDTOMapper

---
+++ src/main/java/com/alvarx4z/woja/infrastructure/controller/dto/mapper/ExpansionDTOMapper.java
@@ -0,0 +1,24 @@
+package com.alvarx4z.woja.infrastructure.controller.dto.mapper;
+
+import com.alvarx4z.woja.domain.Expansion;
+import com.alvarx4z.woja.infrastructure.controller.dto.response.ExpansionResponseDTO;
+import com.alvarx4z.woja.infrastructure.controller.dto.response.ExpansionsResponseDTO;
+
+import java.util.Arrays;
+
+public final class ExpansionDTOMapper {
+
+    public static ExpansionsResponseDTO toResponse(Expansion[] expansions) {
+        return ExpansionsResponseDTO.of(
+            Arrays
+                .stream(expansions)
+                .map(
+                    exp -> ExpansionResponseDTO.of(
+                        String.valueOf(exp.getOrder().getValue()),
+                        String.valueOf(exp.getYear().getValue()),
+                        exp.getTitle().getValue()
+                    )
+                ).toList()
+        );
+    }
+}
