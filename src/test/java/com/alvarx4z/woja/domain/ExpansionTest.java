1b0c6ae572f283ca7981f191c3e00dc7bd5c3a00
Álvaro de Francisco <alfrasan@gmail.com>
2024-04-05 11:25:25 +0200

    Add Unit Test

--- src/test/java/com/alvarx4z/woja/domain/ExpansionTest.java
+++ src/test/java/com/alvarx4z/woja/domain/ExpansionTest.java
@@ -4,8 +4,11 @@ import com.alvarx4z.woja.domain.shared.Name;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.Year;
+import java.util.Arrays;
 import java.util.UUID;
 
+import static com.alvarx4z.woja.domain.Expansion.VANILLA;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -14,13 +17,15 @@ final class ExpansionTest {
     @Test
     @DisplayName("Should contain valid data in the Expansion Enum")
     void givenExpansions_whenCheckingProperties_thenAreValid() {
-        Expansion expansion = Expansion.valueOf(Expansion.VANILLA.toString());
+        Expansion expansion = Expansion.valueOf(VANILLA.toString());
 
         assertThat(expansion).isInstanceOf(Expansion.class);
         assertThat(expansion.getId()).isInstanceOf(UUID.class);
-        assertThat(expansion.getOrder()).isEqualTo(Expansion.VANILLA.getOrder());
+        assertThat(expansion.getOrder()).isEqualTo(VANILLA.getOrder());
         assertThat(expansion.getTitle()).isInstanceOf(Name.class);
-        assertThat(expansion.getTitle().getValue()).isEqualTo(Expansion.VANILLA.getTitle().getValue());
+        assertThat(expansion.getTitle().getValue()).isEqualTo(VANILLA.getTitle().getValue());
+        assertThat(expansion.getYear()).isInstanceOf(Year.class);
+        assertThat(expansion.getYear().getValue()).isEqualTo(VANILLA.getYear().getValue());
     }
 
     @Test
@@ -29,6 +34,17 @@ final class ExpansionTest {
         int numberOfExpansions = Expansion.values().length;
 
         assertThat(numberOfExpansions).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("Should be all the Expansions' Year from 2004 onwards")
+    void givenExpansions_whenCheckingYears_thenAreValid() {
+        boolean result =
+            Arrays
+                .stream(Expansion.values())
+                .allMatch(exp -> exp.getYear().isAfter(Year.of(VANILLA.getYear().getValue() - 1)));
+
+        assertThat(result).isTrue();
     }
 
     @Test
